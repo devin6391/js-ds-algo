d2c0967ed4776c8a38884d4b564c0adb5f3b0837
vineet.dev@snapdeal.com <vineet.dev@vineet-latitude-e5450.jasperindia.local>
2017-03-01 21:03:54 +0530

    linked list done
    Signed-off-by: vineet.dev@snapdeal.com <vineet.dev@vineet-latitude-e5450.jasperindia.local>

---
+++ linked-list.js
@@ -0,0 +1,90 @@
+var _head = Symbol("head");
+var _count = Symbol("count");
+class LinkedList {
+  constructor() {
+    this[_count] = 0;
+    this[_head] = null;
+  }
+
+  getCount() {
+    return this[_count];
+  }
+
+  addFirst(val) {
+    var node = {
+      data: val,
+      next: null
+    }
+    var headNode = this[_head];
+    if(headNode) {
+      node.next = headNode;
+    }
+    this[_head] = node;
+    this[_count]++;
+  }
+
+  add(val, index) {
+    var node = {
+      data: val,
+      next: null
+    }
+    if(index == 0) {
+      this.addFirst(val);
+      return;
+    }
+    if(index > 0 && index < this[_count]) {
+      var current = this[_head];
+      var previous = null;
+      for(var i = 0; i < index; i++) {
+        previous = current;
+        current = current.next;
+      }
+      node.next = current;
+      previous.next = node;
+      this[_count]++;
+    }
+  }
+
+  removeFirst() {
+    var current = this[_head];
+    if(this[_head]){
+      this[_head] = this[_head].next;
+      this[_count]--;
+      return current.data;
+    } else {
+      return null;
+    }
+  }
+
+  removeAt(index) {
+    var current = this[_head];
+    var size = this[_count];
+    var previous = null;
+    if(index == 0) {
+      return this.removeFirst();
+    } else if(index > 0 && index < size) {
+      for(var i = 0; i < index; i++) {
+        previous = current;
+        current = current.next;
+      }
+      previous.next = current.next;
+      this[_count]--;
+      return current.data;
+    } else {
+      return null;
+    }
+  }
+
+  displayAll() {
+    var current = this[_head];
+    var size = this[_count];
+    var arr = [];
+    for(var i = 0; i < size; i++) {
+      arr.push(current.data);
+      current = current.next;
+    }
+    return arr;
+  }
+}
+
+module.exports = LinkedList;
--- use-ds.js
+++ use-ds.js
@@ -1,7 +1,9 @@
 let repl = require('repl');
 var Stack = require("./stack.js");
 var Queue = require("./queue.js");
+var LinkedList = require("./linked-list.js")
 
 let context = repl.start('> ').context;
 context.Stack = Stack;
 context.Queue = Queue;
+context.LinkedList = LinkedList;
