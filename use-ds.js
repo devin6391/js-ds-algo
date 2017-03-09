b27d9a4c285ba1febda340dd5375b5f38c9843fc
Vineet Dev <vineetdev2008@gmail.com>
2017-03-10 01:31:21 +0530

    adding comment
    Signed-off-by: Vineet Dev <vineetdev2008@gmail.com>

--- use-ds.js
+++ use-ds.js
@@ -4,6 +4,7 @@ const Queue = require("./queue.js");
 const LinkedList = require("./linked-list.js");
 const DLinkedList = require("./d-linked-list.js");
 
+//creating a context and adding all data structures to this context
 let context = repl.start('> ').context;
 context.Stack = Stack;
 context.Queue = Queue;
